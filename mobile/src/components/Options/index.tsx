f2f55187feccd3691cccdd515812ea053d8d14e8
João Alves <alvezsjv@gmail.com>
2022-05-18 12:57:33 -0300

    feat(mobile): add form features and api communication

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -3,12 +3,17 @@ import { View, Text } from 'react-native';
 
 
 import { Copyright } from '../Copyright';
+import { Option } from '../Option';
+import { FeedbackType } from '../Widget';
 
 import { feedbackTypes } from '../../utils/feedbackTypes'
 import { styles } from './styles'
-import { Option } from '../Option';
 
-export function Options() {
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -24,6 +29,7 @@ export function Options() {
                 key={key}
                 title={value.title}
                 image={value.image}
+                onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
               />
             ))
         }
